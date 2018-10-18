af315941ebad0124f8af39ad914ac725c1788908
Andrew ZONG <a.zong@f5.com>
2018-10-18 18:51:51 +0800

    init commit

---
+++ app.py
@@ -0,0 +1,47 @@
+from oslo_config import cfg
+from oslo_config import generator
+
+import sys
+
+'''
+https://docs.openstack.org/oslo.config/latest/
+
+This program demonstrates:
+    1. arguments from config files.
+    2. arguments from command line.
+    3. saving arguments into files.
+
+'''
+
+opt_group = cfg.OptGroup(name="simple", title="A simple example")
+simple_opts = [
+    cfg.BoolOpt("enable", default=False, help=('True enables, False disables'))
+]
+
+cli_opts = [
+    cfg.StrOpt("file", default="gen.conf", help="the config file to save as.")
+]
+
+CONF = cfg.CONF
+
+CONF.register_group(opt_group)
+CONF.register_opts(simple_opts, opt_group)
+CONF.register_cli_opts(cli_opts)
+
+def list_opts():
+    return [
+        ('simple', simple_opts),
+        ('cli', cli_opts)
+    ]
+
+if __name__ == "__main__":
+
+    #CONF(default_config_files=['app.conf'])
+    CONF(sys.argv[1:] + ['--config-file', 'app.conf'])
+
+    print(CONF.simple.enable)
+    print(CONF.file)
+
+    # only this registered namespace can work..
+    # oslo-config-generator --namespace oslo.messaging
+    generator.main(args=['--namespace', 'oslo.messaging', '--output-file', CONF.file])
