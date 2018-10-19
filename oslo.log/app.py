8a471cd81820ca1aff5645ae8a0b1b1b9b0d72ed
Andrew ZONG <a.zong@f5.com>
2018-10-19 18:30:46 +0800

    finish oslo.log

---
+++ oslo.log/app.py
@@ -0,0 +1,21 @@
+
+'''
+very similar with python standard logging
+'''
+
+from oslo_config import cfg
+from oslo_log import log as logging
+
+LOG = logging.getLogger(__name__)
+CONF = cfg.CONF
+DOMAIN = "demo"
+
+######### the differences
+logging.register_options(CONF)
+logging.setup(CONF, DOMAIN)
+#########
+
+# Oslo Logging uses INFO as default
+LOG.info("Oslo Logging")
+LOG.warning("Oslo Logging")
+LOG.error("Oslo Logging")
