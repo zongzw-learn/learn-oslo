943b4916b21da4f8d9aeb8b557e27bd4135e7c9d
Andrew Zong <a.zong@f5.com>
2018-12-24 17:23:13 +0800

    Add pbr

---
+++ pbr/setup.py
@@ -0,0 +1,14 @@
+
+'''
+
+whole bundle of classifiers:
+https://pypi.python.org/pypi?%3Aaction=list_classifiers
+
+trove and python
+https://stackoverflow.com/questions/9094220/trove-classifiers-definition
+
+'''
+
+import setuptools
+
+setuptools.setup(setup_requires=['pbr'], pbr=True)
